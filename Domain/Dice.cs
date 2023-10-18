7ccb1ce88a3208004d106afaadc761865a54c897
Sarah Eggermont <sarah.eggermont@student.hogent.be>
2023-10-18 11:22:58 +0200

    Implement Domain

---
+++ Domain/Dice.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+internal class Dice
+{
+    private readonly Random _randomizer = new();
+
+    internal int Dots { get; private set; }
+
+    internal Dice()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void Roll()
+    {
+        Dots = _randomizer.Next(1, 7);
+    }
+}
